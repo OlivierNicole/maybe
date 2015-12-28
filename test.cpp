ca2aa70c0e9270c9797e5aa6080aa196c918b28f
Olivier Nicole <nicole@ensta.fr>
2015-12-28 18:23:21 +0100

    Prettier test.

--- test.cpp
+++ test.cpp
@@ -6,12 +6,15 @@
 
 int main()
 {
+   using namespace std::placeholders;
+
    std::vector<Maybe<int>> v{Maybe<int>(), Maybe<int>(42), Maybe<int>(13)};
    std::vector<Maybe<bool>> w(3, Maybe<bool>());
 
    constexpr auto is42 = [](int n) { return n == 42 ? true : false; };
-   std::transform(v.begin(), v.end(), w.begin(),
-                  std::bind(&Maybe<int>::map<decltype(is42)>, std::placeholders::_1, is42));
+   std::transform( v.begin(), v.end(), w.begin()
+                 , std::bind(&Maybe<int>::map<decltype(is42)>, _1, is42)
+                 );
    for(const Maybe<bool>& m : w)
    {
       std::cout << m << std::endl;
